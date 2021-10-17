2799a3787c73eccd3f644cffa1524d9fbfe5880d
Xin Wen <xinw@resonatesolutions.com.au>
2021-10-17 21:17:54 +1100

    Data: add function to handle search

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -10,6 +10,8 @@ namespace OdeToFood.Data
     public interface IRestaurantData
     {
         IEnumerable<Restaurant> GetAll();
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+
     }
 
     public class InMemoryRestaurantData : IRestaurantData
@@ -19,14 +21,23 @@ namespace OdeToFood.Data
         {
             restaurants = new List<Restaurant>()
             {
-                new Restaurant {Id = 1, Name = "My Restaurant 1", Location = "Maryland 1", Cuisine = CuisineType.Italian },
-                new Restaurant {Id = 2, Name = "My Restaurant 2", Location = "Maryland 2", Cuisine = CuisineType.Mexican },
-                new Restaurant {Id = 3, Name = "My Restaurant 3", Location = "Maryland 3", Cuisine = CuisineType.Indian }
+                new Restaurant {Id = 1, Name = "Luna's Restaurant", Location = "Maryland 1", Cuisine = CuisineType.Italian },
+                new Restaurant {Id = 2, Name = "Andy's Restaurant", Location = "Maryland 2", Cuisine = CuisineType.Mexican },
+                new Restaurant {Id = 3, Name = "Jane's Restaurant", Location = "Maryland 3", Cuisine = CuisineType.Indian }
             };
         }
         public IEnumerable<Restaurant> GetAll()
         {
             return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+
+        IEnumerable<Restaurant> IRestaurantData.GetRestaurantsByName(string name)
+        {
+            return from r in restaurants
+                   // return all the restaurant data if search string is null
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
                    orderby r.Name
                    select r;
         }
