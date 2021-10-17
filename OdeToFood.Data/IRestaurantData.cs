02b11186876beaa8a08c68e4a802e4b9b9420008
Xin Wen <xinw@resonatesolutions.com.au>
2021-10-17 14:35:39 +1100

    Add project files.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "My Restaurant 1", Location = "Maryland 1", Cuisine = CuisineType.Italian },
+                new Restaurant {Id = 2, Name = "My Restaurant 2", Location = "Maryland 2", Cuisine = CuisineType.Mexican },
+                new Restaurant {Id = 3, Name = "My Restaurant 3", Location = "Maryland 3", Cuisine = CuisineType.Indian }
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
