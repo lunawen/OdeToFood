abeb6b7b2f5f223db6ba09752a24f762b6c0209a
Xin Wen <xinw@resonatesolutions.com.au>
2021-10-19 22:13:58 +1100

    add form control to display data

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -12,16 +13,21 @@ namespace OdeToFood.Pages.Restaurants
     public class EditModel : PageModel
     {
         public Restaurant Restaurant { get; set; }
-        private readonly IRestaurantData restaurantData;
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
 
-        public EditModel(IRestaurantData restaurant)
+        private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
+
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
         {
-            this.restaurantData = restaurant;
+            this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
         }
         
         public IActionResult OnGet(int restaurantId)
         {
             Restaurant = restaurantData.GetRestaurantById(restaurantId);
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
             if (Restaurant == null)
                 return RedirectToPage("./NotFound");
             return Page();
