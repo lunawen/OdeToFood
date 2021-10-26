3db4a51aed598a64e0cc387f92756af3c8acf18a
Luna Wen <lunawen33@gmail.com>
2021-10-26 20:48:46 +1100

    enable Api controller

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -28,6 +28,7 @@ namespace OdeToFood
             services.AddDbContextPool<OdeToFoodDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("OdeToFood")));
             services.AddScoped<IRestaurantData, SqlRestaurantData>();
             services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +55,7 @@ namespace OdeToFood
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
