5ed9bd3d92c69b121ab70ad38d8522755b2ee2ad
Luna Wen <lunawen33@gmail.com>
2021-10-27 22:54:38 +1100

    add custom middleware

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -44,7 +45,7 @@ namespace OdeToFood
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.Use(SayHelloDelegate);
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -57,6 +58,17 @@ namespace OdeToFood
                 endpoints.MapRazorPages();
                 endpoints.MapControllers();
             });
+        }
+
+        private RequestDelegate SayHelloDelegate(RequestDelegate arg)
+        {
+            return async ctx =>
+            {
+                if (ctx.Request.Path.StartsWithSegments("/hello"))
+                    await ctx.Response.WriteAsync("Hello, world");
+                else
+                    await arg(ctx);
+            };
         }
     }
 }
